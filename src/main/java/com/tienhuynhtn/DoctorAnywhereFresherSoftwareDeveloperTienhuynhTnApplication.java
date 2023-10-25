6b66df24c4bab488ffc17c3c544605d5503023c7
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2023-10-25 23:40:03 +0700

    [tienhuynh-tn] Init project - first commit

---
+++ src/main/java/com/tienhuynhtn/DoctorAnywhereFresherSoftwareDeveloperTienhuynhTnApplication.java
@@ -0,0 +1,13 @@
+package com.tienhuynhtn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAnywhereFresherSoftwareDeveloperTienhuynhTnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorAnywhereFresherSoftwareDeveloperTienhuynhTnApplication.class, args);
+    }
+
+}
